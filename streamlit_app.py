e2d8cce1f6a9d7efd913aee4474ec8763d7be7b1
Fastcbr10 <fastcbr10@gmail.com>
2025-03-11 19:19:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-$ pip install -r requirements.txt
+
 st.title("🎈 My new Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
